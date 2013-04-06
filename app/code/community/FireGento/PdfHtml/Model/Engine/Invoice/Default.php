08602e5c1c803efbf9dd3abc0ef95d63e31f3a48
Gerrit Pechmann <gp@aijko.de>
2013-04-06 14:01:27 -0700

    Initial commit.

---
+++ app/code/community/FireGento/PdfHtml/Model/Engine/Invoice/Default.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This file is part of the FIREGENTO project.
+ *
+ * FireGento_GermanSetup is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This script is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * PHP version 5
+ *
+ * @category  FireGento
+ * @package   FireGento_Pdf
+ * @author    FireGento Team <team@firegento.com>
+ * @copyright 2013 FireGento Team (http://www.firegento.de). All rights served.
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ * @version   $Id:$
+ * @since     0.1.0
+ */
+/**
+ * Html2Pdf invoice rendering engine.
+ *
+ * @category  FireGento
+ * @package   FireGento_Pdf
+ * @author    FireGento Team <team@firegento.com>
+ * @copyright 2013 FireGento Team (http://www.firegento.de). All rights served.
+ * @license   http://opensource.org/licenses/gpl-3.0 GNU General Public License, version 3 (GPLv3)
+ * @version   $Id:$
+ * @since     0.1.0
+ */
+class FireGento_PdfHtml_Model_Engine_Invoice_Default
+{
+    /**
+     * Return pdf file.
+     *
+     * @param array $invoices
+     */
+    public function getPdf($invoices = array())
+    {
+        foreach ($invoices as $invoice) {
+            if ($invoice->getStoreId()) {
+                Mage::app()->getLocale()->emulate($invoice->getStoreId());
+                Mage::app()->setCurrentStore($invoice->getStoreId());
+            }
+
+
+            $order = $invoice->getOrder();
+
+
+            $html = '<html><body><h1>Test</h1></body></html>';
+
+        }
+    }
+}
